50f07ef7f4d802dc584afb687a75b3e35df4ea79
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-01-03 13:18:24 -0500

    create article successfully

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,24 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    
+    @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:success] = 'Article has been created'
+      redirect_to articles_path
+    else
+      flash[:failure] = 'Something went wrong'
+    end
+
+  end
+end
+
+private
+
+def article_params
+  params.require(:article).permit(:title, :body)
 end
