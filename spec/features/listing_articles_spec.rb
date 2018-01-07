efa75f2205ad0a7d96794d47c8f004960faba986
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-01-07 15:47:09 -0500

    show new article button to signe in users

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "Listing articles" do
   before do
-    john = User.create(email: 'john@example.com', password: 'password')
+    @john = User.create(email: 'john@example.com', password: 'password')
 
-    @article1 = Article.create(title: "The first article", body: "Lorem ispsum dolor sit amet, consectetur.", user: john)
+    @article1 = Article.create(title: "The first article", body: "Lorem ispsum dolor sit amet, consectetur.", user: @john)
 
-    @article2 = Article.create(title: "The second article", body: "Body of 2nd article", user: john)
+    @article2 = Article.create(title: "The second article", body: "Body of 2nd article", user: @john)
   end
 
   scenario "with articles created and user not signed in" do
@@ -22,6 +22,22 @@ RSpec.feature "Listing articles" do
     expect(page).to have_link(@article2.title)
 
     expect(page).not_to have_link("New Article")
+  end
+
+  scenario "with articles created and user signed in" do
+    login_as(@john)
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+
+    expect(page).to have_link("New Article")
   end
 
   scenario "A user has no articles" do
